03be6b573f2a4b742aaca462df8f3bdf2c0a62d7
Laura Rose Mary Hansen <laurarosemaryhansen@gmail.com>
2021-12-15 14:34:52 +0000

    plswork

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 
 app.use(express.json())
 app.use(router)
-const uri = "mongodb+srv://mongo-colin:p4ssw0rd@cluster0.ywlkz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+const uri = "mongodb+srv://anon45:IHATEDATABASES@cluster0.invl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(uri);
 
 app.listen(port, () =>{
