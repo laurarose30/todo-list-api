3f2d4813a507ff2fb5610c9d11d9b7b518acbd1b
compinschools <colinsmith@smithcolin.co.uk>
2021-12-15 12:08:20 +0000

    added mongodb

--- todosController.js
+++ todosController.js
@@ -1,69 +1,111 @@
 const createError = require("http-errors")
+const  ObjectId = require('mongodb').ObjectId;
 let todoList = []
 let idno = 0
-exports.index = function(req,res){
-    res.send(todoList)
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://mongo-colin:p4ssw0rd@cluster0.ywlkz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const uri = "mongodb+srv://Sazzle:sazzlemongo1@cluster0.opbm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+exports.index = async function(req,res){
+   
+    client.connect(async (err) => {
+        const collection = client.db("bliss-bakery").collection("todo");
+        // perform actions on the collection object
+        
+        const findPromise = collection.find()
+        res.send(await findPromise.toArray())
+        
+        client.close();
+      });
 }
 
-exports.create = function (req,res,next){
+exports.create = async function (req,res,next){
     if(!req.body.name){
         return (next(createError(400,"name is required")))
     }
 
-    todoList.push({
+    client.connect(async (err) => {
+        const collection = client.db("bliss-bakery").collection("todo");
+        // perform actions on the collection object
+        collection.insertOne(
+            {
+                name: req.body.name,
+                description: req.body.description
+            }
+        )
+        .then(() => res.send({result: "true"}))
+        
+        client.close();
+    });
+    
+    /*todoList.push({
         id: idno,
         name: req.body.name,
         description: req.body.description
-    });
+    });*/
     idno++;
-    res.send({result: "true"})
+    //res.send({result: "true"})
 }
 
 exports.show = function (req,res,next) {
+
+    client.connect(async (err) => {
+        const collection = client.db("bliss-bakery").collection("todo");
+        // perform actions on the collection object
+        collection.findOne({ _id: ObjectId( req.params.id)})
+        .then( (todoItem) => {
+            if(!todoItem){
+            return (next(createError(404,"no todo with that id")))
+            }
+            res.send(todoItem);
+
+        })
+        .catch ( err => console.log(err))
+
+        client.close();
+});  /*
     const todoItem = todoList.find( (item) => item.id == req.params.id )
     if(!todoItem) {
         return (next(createError(404,"no todo with that id")))
     }
 
-    res.send(todoItem)
+    res.send(todoItem)*/
 
 }
 
-exports.update = function (req,res,next){
-    //verifying
-    const todoItem = todoList.find( (item) => item.id == req.params.id )
-    
-    if(!req.body.name){
-        return (next(createError(400,"name is required")))
-    }
+exports.update = async function (req,res,next){
 
-    if(!todoItem) {
-        return (next(createError(404,"no todo with that id")))
-    }
-//updating
-    todoList = todoList.map ((item) => {
-        if(item.id == req.params.id){
-            item.name = req.body.name,
-            item.description = req.body.description
+    client.connect(async (err) => {
+        const collection = client.db("bliss-bakery").collection("todo");
+        // perform actions on the collection object
+        const r = await (await collection).updateOne({ _id: ObjectId(req.params.id)},{ $set: {name:req.body.name, description:req.body.description,completed: req.body.completed}})
+        if(r.matchedCount){
+            return res.send({result:true})
         }
-        
-        return item;
+        return (next(createError(404,"no todo with that id")))
 
-    } )
-
-    res.send({result: true})
+        client.close();
+}); 
+    
 }
 
 exports.delete = function (req,res,next) {
     //verifying
-    const todoItem = todoList.find( (item) => item.id == req.params.id )
-
-    if(!todoItem) {
-        return (next(createError(404,"no todo with that id")))
-    }
-
-    //deleting
-    todoList = todoList.filter( (item) => item.id != req.params.id)
-    res.send({result:true})
+    client.connect(async (err) => {
+        const collection = client.db("bliss-bakery").collection("todo");
+        // perform actions on the collection object
+        collection.deleteOne({ _id: ObjectId(req.params.id)})
+        .then ((r) => {
+            if(r.deletedCount){
+                return res.send({result:true});
+            }
+            return (next(createError(404,"no todo with that id")))
+        })
+        client.close();
+    }); 
+    
+    
 }
